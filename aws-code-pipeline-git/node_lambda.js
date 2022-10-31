23900175933d346df144d85a4e7834eda9348604
Urvesh Gayakwad <57410418+Urvesh05@users.noreply.github.com>
2022-10-31 14:53:30 +0530

    Update node_lambda.js

--- aws-code-pipeline-git/node_lambda.js
+++ aws-code-pipeline-git/node_lambda.js
@@ -1,18 +1,32 @@
-exports.handler = async function(event, context, callback) {
-   let arrItems = [4,5,6,8,9,10,35,70,80,31];
-   function countevennumbers (items) {
-      return new Promise(resolve => {
-         setTimeout(() => {
-            let a = 0;
-            for (var i in items) {
-               if (items[i] % 2 == 0) {
-                  a++;
-               } 
-            }
-            resolve(a);
-         }, 2000);
-      });
-   }
-   let evennumber = await countevennumbers(arrItems);
-   callback(null,'even numbers equals ='+evennumber);
-};
+// exports.handler = async function(event, context, callback) {
+//    let arrItems = [4,5,6,8,9,10,35,70,80,31];
+//    function countevennumbers (items) {
+//       return new Promise(resolve => {
+//          setTimeout(() => {
+//             let a = 0;
+//             for (var i in items) {
+//                if (items[i] % 2 == 0) {
+//                   a++;
+//                } 
+//             }
+//             resolve(a);
+//          }, 2000);
+//       });
+//    }
+//    let evennumber = await countevennumbers(arrItems);
+//    callback(null,'even numbers equals ='+evennumber);
+// };
+
+
+exports.handler = async (event) =>
+{
+    const min=1;
+    const max=6;
+    
+    const ramdomNumber = Math.floor(
+        Math.random() * (max-min +1)
+    )+min;
+    const message = 'This is a Random Number ' + ramdomNumber;
+    
+    return message;
+}
