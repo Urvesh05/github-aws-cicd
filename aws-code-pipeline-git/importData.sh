a49f95fe94761ebcf3ba3987dee94f00095ec07a
Urvesh Gayakwad <57410418+Urvesh05@users.noreply.github.com>
2022-11-21 13:56:22 +0530

    Update importData.sh

--- aws-code-pipeline-git/importData.sh
+++ aws-code-pipeline-git/importData.sh
@@ -1,16 +1,20 @@
 start_time="$(date -u +%s)"
 aws_region_name=$2
+echo " data $test"
 arr=("test" "test2")
-for folder_path in ${arr[@]}
-do
-    #import all data in the folder
-    for filename in $folder_path/ScriptForDataImport/*.txt; do
-        echo "importing ${filename}"
-        aws dynamodb batch-write-item --request-items file://${filename}
+if[$test == "test"]
+then
+    for folder_path in ${arr[@]}
+    do
+        #import all data in the folder
+        for filename in $folder_path/ScriptForDataImport/*.txt; do
+            echo "importing ${filename}"
+            aws dynamodb batch-write-item --request-items file://${filename}
+        done
+        echo "Completed"    
+        ##script run time
+        end_func_time="$(date -u +%s)"
+        echo "Total of $(($end_func_time-$start_time)) seconds to completed the function"
+        # rm -rf $folder_path/
     done
-    echo "Completed"    
-    ##script run time
-    end_func_time="$(date -u +%s)"
-    echo "Total of $(($end_func_time-$start_time)) seconds to completed the function"
-    # rm -rf $folder_path/
-done
+fi
